70d6c9f03dec5f1876cfb97215abaf32c08bf918
김호진 <gimhojin@gimhojin-ui-MacBookAir.local>
2021-07-09 20:32:45 +0900

    class studying

---
+++ Study/app/src/main/java/com/example/study/FruitKotlin.kt
@@ -0,0 +1,4 @@
+package com.example.study
+
+data class FruitKotlin(var fruitName:String, var desscription:String) {
+}
---
+++ Study/app/src/main/java/com/example/study/exString.kt
@@ -0,0 +1,5 @@
+package com.example.study
+
+fun String.lsatString():String {
+    return this.get(this.length - 1).toString()
+}
